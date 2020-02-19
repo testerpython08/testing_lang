e13717db67450fdf0dc4e45eb3f8294a137c384f
Aduchiev Vitaly <vaduchiev@mtt.ru>
2020-02-19 18:24:23 +0300

    Added class of locators for main page and modified body of method should_be_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,3 @@
-import time
-
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,7 +5,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(10)
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"

147d10c754b925240863faf8052e023cc4cfbb25
Aduchiev Vitaly <vaduchiev@mtt.ru>
2020-02-19 15:42:29 +0300

    Add requirements.txt, test_main_page.py has test that goes to main page as guest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,9 @@
-# @pythontester08
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption("--language", action='store', default=None,
+    parser.addoption("--language", action='store', default="en",
                      help='Choose language for making shopping')
 
 # принимает параметр для изменения языка
@@ -14,6 +13,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option("prefs",
                                     {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(options=options, executable_path="C:\webdrivers\chromedriver.exe")
     yield browser
     browser.quit()

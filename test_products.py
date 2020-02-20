ea503158c62a30bf88de46431492f33d61fa4f0d
Aduchiev Vitaly <vaduchiev@mtt.ru>
2020-02-20 18:30:14 +0300

    Mark one link as xfail

--- test_products.py
+++ test_products.py
@@ -8,7 +8,7 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
